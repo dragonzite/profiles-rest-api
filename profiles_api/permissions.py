6ef414455626abbb91f4ae72a3a32fa068c72287
dragonzite <dragonzite@gmail.com>
2020-07-19 14:21:28 -0700

    Added login API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,21 +1,11 @@
 from rest_framework import permissions
 
+
 class UpdateOwnProfile(permissions.BasePermission):
-    """Allow users to edit their own profile
+    """Allow users to edit their own profile"""
 
-    Keyword arguments: none, base
-    argument -- description
-    Return: return_description
-    """
-
-    def has_object_permissions(self, request, view, obj):
-        """Check user is trying to edit their own profile
-
-        Args:
-            request (info): requset informetion
-            view (obj): the view
-            obj (obj): the object
-        """
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
 
